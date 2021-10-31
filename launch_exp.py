7374b308b75a6f716a32494ea250d4f316d852a2
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2021-10-31 20:50:00 +0100

    fix launch exp script

--- launch_exp.py
+++ launch_exp.py
@@ -10,7 +10,7 @@ nthreads = int(sys.argv[2])
 config_files = [os.path.join(config_path, f) for f in os.listdir(config_path) if os.path.isfile(os.path.join(config_path, f))]
 
 def launch_rust(config):
-    subprocess.run(['./target/debug/consistent-viterbi', '-c', config])
+    subprocess.run(['./target/release/consistent-viterbi', '-c', config, '-e'])
 
 with Pool(nthreads) as p:
     p.map(launch_rust, config_files)

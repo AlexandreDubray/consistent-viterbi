3cfa8581d0bfcefe6773c2e3222580c03c74a3cd
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2021-12-29 10:41:25 +0100

    use tuple for dp table

--- src/viterbi_solver/dp.rs
+++ src/viterbi_solver/dp.rs
@@ -4,42 +4,6 @@ use std::collections::{HashSet, HashMap};
 use super::super::hmm::hmm::HMM;
 use super::utils::SuperSequence;
 
-type Backtrack = (usize, usize, usize);
-
-#[derive(Debug)]
-struct DPEntry {
-    pub time: usize,
-    pub state: usize,
-    pub cstr_paths: HashMap<usize, (f64, Option<Backtrack>)>,
-}
-
-impl DPEntry {
-
-    pub fn new(time: usize, state: usize) -> Self {
-        let cstr_paths: HashMap<usize, (f64, Option<Backtrack>)> = HashMap::new();
-        Self {time, state, cstr_paths}
-    }
-
-    pub fn update(&mut self, cost: f64, from: Option<Backtrack>, cstr: usize) -> bool {
-        let entry = self.cstr_paths.entry(cstr).or_insert((f64::NEG_INFINITY, None));
-        if cost > entry.0 {
-            *entry = (cost, from);
-        }
-        cost > entry.0
-    }
-
-    pub fn len(&self) -> usize {
-        self.cstr_paths.len()
-    }
-}
-
-pub struct DPSolver<'a, const D: usize> {
-    pub solution: Array1<usize>,
-    hmm: &'a HMM<D>,
-    sequence: &'a mut SuperSequence<'a, D>,
-    table: HashMap<(i32, usize), DPEntry>,
-}
-
 fn get_cstr_choice(uid: usize, cstr_id: usize, c: usize) -> usize {
     (uid / c.pow(cstr_id as u32)) % c
 }
@@ -51,47 +15,54 @@ fn update_cstr_choice(uid: usize, cstr_id: usize, choice: usize, c: usize) -> us
 }
 
 
+pub struct DPSolver<'a, const D: usize> {
+    pub solution: Array1<usize>,
+    hmm: &'a HMM<D>,
+    sequence: &'a mut SuperSequence<'a, D>,
+    table: Array1<HashMap<(usize, usize), (usize, usize, f64)>>
+}
+
+
 impl<'b, const D: usize> DPSolver<'b, D> {
 
     pub fn new(hmm: &'b HMM<D>, sequence: &'b mut SuperSequence<'b, D>) -> Self {
         let solution = Array1::from_elem(sequence.len(), 0);
-        let table: HashMap<(i32, usize), DPEntry> = HashMap::new();
+        let table: Array1<HashMap<(usize, usize), (usize, usize, f64)>> = Array1::from_elem(sequence.len(), HashMap::new());
         Self {solution, hmm, sequence, table}
     }
 
-    fn prune(&self, dp_entry: &mut DPEntry, finished_constraints: &Vec<bool>) {
-        let mut to_remove: Vec<usize> = Vec::new();
-        let cstr_node_ids: Vec<&usize> = dp_entry.cstr_paths.keys().collect();
+    fn prune(&mut self, t: usize, finished_constraints: &Vec<bool>) {
+        let mut to_remove: Vec<(usize, usize)> = Vec::new();
+        let keys: Vec<&(usize, usize)> = self.table[t].keys().collect();
         let c = self.hmm.nstates() + 1;
-        for i in 0..cstr_node_ids.len() {
-            for j in i+1..cstr_node_ids.len() {
-                let n1 = cstr_node_ids[i];
-                let n2 = cstr_node_ids[j];
-                
-                let mut same = true;
-                for k in 0..self.sequence.nb_cstr {
-                    if !finished_constraints[k] {
-                        same = get_cstr_choice(*n1, k, c) == get_cstr_choice(*n2, k, c);
-                        if !same {
-                            break;
+        for i in 0..keys.len() {
+            for j in i+1..keys.len() {
+                let (s1, cstr1) = keys[i];
+                let (s2, cstr2) = keys[j];
+                if s1 == s2 {
+                    let mut same = true;
+                    for k in 0..self.sequence.nb_cstr {
+                        if !finished_constraints[k] {
+                            same = get_cstr_choice(*cstr1, k, c) == get_cstr_choice(*cstr2, k, c);
+                            if !same { break; }
                         }
                     }
-                }
-
-                // Only keep the best one
-                if same {
-                    let v1 = dp_entry.cstr_paths.get(n1).unwrap().0;
-                    let v2 = dp_entry.cstr_paths.get(n2).unwrap().0;
-                    if v1 > v2 {
-                        to_remove.push(*n2);
-                    } else {
-                        to_remove.push(*n1);
+                    
+                    if same {
+                        let v1 = self.table[t].get(&keys[i]).unwrap().2;
+                        let v2 = self.table[t].get(&keys[j]).unwrap().2;
+                        if v1 > v2 {
+                            to_remove.push((*s2, *cstr2));
+                        } else {
+                            to_remove.push((*s1, *cstr1));
+                        }
                     }
                 }
             }
         }
+
         for tr in to_remove {
-            dp_entry.cstr_paths.remove(&tr);
+            self.table[t].remove(&tr);
         }
     }
 
@@ -100,14 +71,35 @@ impl<'b, const D: usize> DPSolver<'b, D> {
         // 0, thus we need nstates + 1 integers
         let nb_cstr_choice = self.hmm.nstates() + 1;
 
-        let mut dp_entry_source = DPEntry::new(0, 0);
-        dp_entry_source.update(0.0, None, 0);
-        self.table.insert((-1, 0), dp_entry_source);
-
         let mut finished_constraints: Vec<bool> = (0..self.sequence.nb_cstr).map(|_| false).collect();
         let mut valid_states: Vec<HashSet<usize>> = (0..self.sequence.nb_cstr).map(|_| (0..self.hmm.nstates()).collect()).collect();
 
-        for idx in 0.. self.sequence.len() {
+        {
+            let mut to_insert: Vec<((usize, usize), (usize, usize, f64))> = Vec::new();
+            let first = &self.sequence[0];
+            for state in 0..self.hmm.nstates() {
+                let is_constrained = self.sequence.is_constrained(0);
+                if !first.can_be_emited(self.hmm, state) { continue; }
+                let cost = first.arc_p(self.hmm, 0, state);
+                if cost > f64::NEG_INFINITY {
+                    let cstr = if is_constrained {
+                        let ucomp = first.constraint_component as usize;
+                        update_cstr_choice(0, ucomp, state, nb_cstr_choice)
+                    } else {
+                        0
+                    };
+                    to_insert.push(((state, cstr), (0, 0, cost)));
+                }
+            }
+
+            for v in to_insert {
+                let key = v.0;
+                let value = v.1;
+                self.table[0].insert(key, value);
+            }
+        }
+
+        for idx in 1.. self.sequence.len() {
             if idx % 10000 == 0 {
                 println!("{}/{}", idx, self.sequence.len());
             }
@@ -115,7 +107,6 @@ impl<'b, const D: usize> DPSolver<'b, D> {
             // Check if the layer is constrained?
             let is_constrained = self.sequence.is_constrained(idx);
 
-            let mut updated = false;
             for state_to in 0..self.hmm.nstates() {
                 if is_constrained && !valid_states[element.constraint_component as usize].contains(&state_to) {
                     // In a previous layer of the same component, this state was not possible. So even
@@ -129,101 +120,83 @@ impl<'b, const D: usize> DPSolver<'b, D> {
                     }
                     continue;
                 }
-                let mut dp_entry = DPEntry::new(idx, state_to);
-                for state_from in 0..self.hmm.nstates() {
+
+                let mut to_insert: Vec<((usize, usize), (usize, usize, f64))> = Vec::new();
+                for (key, val) in self.table[idx - 1].iter() {
+                    let state_from = key.0;
                     let arc_cost = element.arc_p(self.hmm, state_from, state_to);
                     if arc_cost == f64::NEG_INFINITY { continue; }
-                    match self.table.get_mut(&(idx as i32 - 1, state_from)) {
-                        None => (),
-                        Some(entry) => {
-                            if !is_constrained {
-                                for (cstr_id, value) in &entry.cstr_paths {
-                                    let cost = value.0 + arc_cost;
-                                    let u = dp_entry.update(cost, Some((entry.time, entry.state, *cstr_id)), *cstr_id);
-                                    updated = updated || u;
-                                }
-                            } else {
-                                let ucomp = element.constraint_component as usize;
-                                for (cstr_id, value) in &entry.cstr_paths {
-                                    let choice = get_cstr_choice(*cstr_id, ucomp, nb_cstr_choice);
-                                    if choice == 0 || choice == state_to {
-                                        let cost = value.0 + arc_cost;
-                                        let new_cstr_id = update_cstr_choice(*cstr_id, ucomp, state_to, nb_cstr_choice);
-                                        let u = dp_entry.update(cost, Some((entry.time, entry.state, *cstr_id)), new_cstr_id);
-                                        updated = updated || u;
-                                    }
-                                }
-                            }
+                    let cost = arc_cost + val.2;
+                    let cstr = key.1;
+                    if !is_constrained {
+                        to_insert.push(((state_to, cstr), (state_from, cstr, cost)));
+                    } else {
+                        let ucomp = element.constraint_component as usize;
+                        let choice = get_cstr_choice(ucomp, cstr, nb_cstr_choice);
+                        if choice == 0 {
+                            // No choice made for the constraint component so far
+                            let new_cstr = update_cstr_choice(ucomp, cstr, state_to + 1, nb_cstr_choice);
+                            to_insert.push(((state_to, new_cstr), (state_from, cstr, cost)));
+                        } else if choice == state_to {
+                            to_insert.push(((state_to, cstr), (state_from, cstr, cost)));
                         }
-                    };
+                    }
                 }
 
-                if !updated {
-                    panic!("No possible state at time {} for element {:?}", idx, element);
-                }
-                // Let assume that we saw the last elements of Ci, every path of constraints
-                // looks like
-                // <_, _, ..., i1, ...>
-                // <_, _, ..., i2, ...>
-                // <_, _, ..., i3, ...>
-                // ...
-                // <_, _, ..., in, ...>
-                //
-                // if for two different i's, the rest of the choices are the same, we can
-                // discard the choices with the lowest value (since whatever happens in the
-                // rest of the DAG, it will apply to both choices paths)
-                //
-                // More Generally, if for all unfinished constraints, the choices are the same,
-                // keep only the choice with the best value
-
-                if is_constrained && element.last_of_constraint {
-                    finished_constraints[element.constraint_component as usize] = true;
-                    self.prune(&mut dp_entry, &finished_constraints);
+                let valid_state = to_insert.len() > 0;
+                for v in to_insert {
+                    let key = v.0;
+                    let value = v.1;
+                    let entry = self.table[idx].entry(key).or_insert((0, 0, f64::NEG_INFINITY));
+                    if value.2 > entry.2 {
+                        *entry = value;
+                    }
                 }
 
-                if dp_entry.len() > 0 {
-                    self.table.insert((idx as i32, state_to), dp_entry);
-                } else if is_constrained {
+                if !valid_state {
                     valid_states[element.constraint_component as usize].remove(&state_to);
                 }
+            }
+
+            // Let assume that we saw the last elements of Ci, every path of constraints
+            // looks like
+            // <_, _, ..., i1, ...>
+            // <_, _, ..., i2, ...>
+            // <_, _, ..., i3, ...>
+            // ...
+            // <_, _, ..., in, ...>
+            //
+            // if for two different i's, the rest of the choices are the same, we can
+            // discard the choices with the lowest value (since whatever happens in the
+            // rest of the DAG, it will apply to both choices paths)
+            //
+            // More Generally, if for all unfinished constraints, the choices are the same,
+            // keep only the choice with the best value
+
+            if is_constrained && element.last_of_constraint {
+                finished_constraints[element.constraint_component as usize] = true;
+                self.prune(idx, &finished_constraints);
             }
         }
         self.backtrack();
     }
 
     pub fn backtrack(&mut self) {
-        let mut dp_entry_target: Option<&DPEntry> = None;
-        let mut best_cstr_path: Option<usize> = None;
+        let mut end_state: Option<(usize, usize)> = None;
         let mut best_cost = f64::NEG_INFINITY;
-        for state in 0..self.hmm.nstates() {
-            match self.table.get(&(self.sequence.len() as i32 - 1, state)) {
-                Some(entry) => {
-                    for (cstr_path_idx, value) in &entry.cstr_paths {
-                        if value.0 > best_cost {
-                            best_cost = value.0;
-                            dp_entry_target = Some(entry);
-                            best_cstr_path = Some(*cstr_path_idx);
-                        }
-                    }
-                },
-                None => ()
-            };
+        for (key, val) in self.table[self.sequence.len()-1].iter() {
+            let cost = val.2;
+            if cost > best_cost {
+                end_state = Some(*key);
+                best_cost = cost;
+            }
         }
 
-        let mut current  = dp_entry_target.unwrap();
+        let mut current = end_state.unwrap();
         for idx in (0..self.solution.len()).rev() {
-            self.solution[idx] = current.state;
-            if self.sequence[idx].constraint_component != -1 {
-                let element = &self.sequence[idx];
-                let ucomp = element.constraint_component as usize;
-            }
-            let (_, from) = current.cstr_paths.get(&best_cstr_path.unwrap()).unwrap();
-            if idx != 0 {
-                let from = from.as_ref().unwrap();
-                let key = (from.0 as i32, from.1);
-                best_cstr_path = Some(from.2);
-                current = self.table.get(&key).unwrap();
-            }
+            self.solution[idx] = current.0;
+            let from = self.table[idx].get(&current).unwrap();
+            current = (from.0, from.1);
         }
     }
 
